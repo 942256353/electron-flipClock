230da0d6f8b88f72edeb652b98d98f33bd068b38
942256453 <942256453@qq.com>
2023-09-14 22:24:29 +0800

    --

--- flipClock/src/main/index.ts
+++ flipClock/src/main/index.ts
@@ -42,9 +42,9 @@ function createWindow(): void {
     }
   })
   mainWindow.setIgnoreMouseEvents(true,{forward:true})
-  // if(is.dev){
+   if(is.dev){
     mainWindow.webContents.openDevTools()
-  //}
+  }
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
